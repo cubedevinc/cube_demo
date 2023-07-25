4498ecdf7cc42b98f18bc921ee11f7f88630cdd1
artyom <artyom@cube.dev>
2023-07-25 03:41:13 +0000

    Mask first and last names

--- cube.js
+++ cube.js
@@ -1,9 +1,26 @@
 module.exports = {
+  // Mapping of security contexts to data model versions
+  // https://cube.dev/docs/reference/configuration/config#contexttoappid
+  contextToAppId: ({ securityContext }) => {
+    return securityContext.team
+  },
+  
+  // Security hook which is run before a query is executed
+  // https://cube.dev/docs/reference/configuration/config#queryrewrite
+  queryRewrite: (query, { securityContext }) => {
+    if (!securityContext.team) {
+      securityContext.team = 'public'
+    }
+
+    return query
+  },
+
   semanticLayerSync: () => {
     return [{
       type: "preset",
       name: "Preset Sync",
       config: {
+        database: "Cube Cloud 201",
         api_token: process.env.PRESET_API_TOKEN,
         api_secret: process.env.PRESET_API_SECRET,
         workspace_url: process.env.PRESET_WORKSPACE_URL
