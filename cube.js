e91dbac1dcf7c34f77a7d321aa0075e9dcc081f5
tony <tony@cube.dev>
2023-08-28 16:28:17 +0000

    remove SLS

--- cube.js
+++ cube.js
@@ -24,18 +24,7 @@ module.exports = {
       password: process.env.CUBEJS_SQL_PASSWORD,
       securityContext: securityContext,
     };
-  },
-
-  semanticLayerSync: () => {
-    return [{
-      type: "preset",
-      name: "Preset Sync",
-      config: {
-        database: "Cube Cloud 201",
-        api_token: process.env.PRESET_API_TOKEN,
-        api_secret: process.env.PRESET_API_SECRET,
-        workspace_url: process.env.PRESET_WORKSPACE_URL
-      }
-    }];
   }
+
+  
 };
