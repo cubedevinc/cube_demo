a7072867a8d1073fd19847607234cedb41394b5d
tony <tony@cube.dev>
2023-09-07 16:12:05 +0000

    asdf

--- cube.js
+++ cube.js
@@ -4,18 +4,19 @@ module.exports = {
   contextToAppId: ({
     securityContext
   }) => {
-    return securityContext.team;
+    //return securityContext.team;
+    return securityContext;
   },
   // Security hook which is run before a query is executed
   // https://cube.dev/docs/reference/configuration/config#queryrewrite
-  queryRewrite: (query, {
-    securityContext
-  }) => {
-    if (!securityContext.team) {
-      securityContext.team = 'public';
-    }
-    return query;
-  },
+  // queryRewrite: (query, {
+  //   securityContext
+  // }) => {
+  //   if (!securityContext.team) {
+  //     securityContext.team = 'public';
+  //   }
+  //   return query;
+  // },
   checkSqlAuth: (query, username) => {
     const securityContext = {
       team: username
@@ -31,8 +32,8 @@ module.exports = {
       name: "Preset Sync",
       config: {
         database: "828 Take 2",
-        api_token: "8623f3be-8ab7-466c-a8c3-a312ba440864",
-        api_secret: "3bb335ac0b1208350e592f99df007db4f19f3aa81e4e3cecf6202601a0c53eba",
+        api_token: "448d9518-3d7a-41e3-9bda-e626b1646c32",
+        api_secret: "e5e5374a64e12b0f7907e417f0d93ad11e7cbe0046181ba1896671d9949c7120",
         workspace_url: "5276833d.us2a.app.preset.io"
       }
     }];
