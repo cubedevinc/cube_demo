89d5d1bc7a5700fab12e8851971dd2c99e19dfcf
tony <tony@cube.dev>
2024-01-18 17:43:57 +0000

    asdf

---
+++ cube.js
@@ -0,0 +1,19 @@
+module.exports = {
+  semanticLayerSync: () => {
+    return [{
+      type: "preset",
+      name: "Preset Sync",
+      config: {
+        api_token: process.env.PRESET_API_TOKEN,
+        api_secret: process.env.PRESET_API_SECRET,
+        workspace_url: process.env.PRESET_WORKSPACE_URL
+      }
+    }, {
+      type: "powerbi",
+      name: "Powerbi Sync",
+      config: {
+        database: "Cube Cloud: cube_demo"
+      }
+    }];
+  }
+};
